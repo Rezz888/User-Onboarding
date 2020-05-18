2bac2450faf4f238555b7f7625a945d76dbdbae5
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-05-17 22:00:03 -0400

    Test with Cypress

--- my-app/cypress/integration/form_test.js
+++ my-app/cypress/integration/form_test.js
@@ -10,10 +10,38 @@ describe("Testing our forms inputs", () => {
         cy.visit("http://localhost:3000/");
     });
 
-    it("Finds the name input", () => {
-        // Arrange - get the element
-        cy.get('[for="name"]')
-        // Act - Mimic user interaction
-        // Assert - Test/Verify
-    })
-})
+    //pulling url from beforeEach
+    it("Input name into the name input", () => {
+        //Arrange - Get the element
+        //Act - Mimic user interaction
+        //Assert - Test / Verify
+        cy.get('input[name="name"]')
+        .type("Rezaur Rahman")
+        .should("have.value", "Rezaur Rahman")
+
+        cy.get('input[name="email"]')
+        .type("waffle@lala.com")
+        .should("have.value", "waffle@lala.com")
+        cy.contains("Email is taken");
+        cy.get('input[name="email"]')
+        .clear();
+        cy.contains("Must include email address");
+        cy.get('input[name="email"]')
+        .type("razeeeb@gmail.com")
+        .should("have.value", "razeeeb@gmail.com")
+
+        cy.get('input[name="password"]')
+        
+
+        cy.get('select').select('Teacher')
+       
+
+        cy.get('input[type="checkbox"]').check().should("be.checked");
+
+        cy.get("form").submit();
+
+        cy.get('.users')
+        .should('exist')
+        
+    });
+});
